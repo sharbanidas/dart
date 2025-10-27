ab78d0e6aa2af879940f5363881e39fe6846d004
mouchowdhury <chowdhurymou380@gmail.com>
2025-10-27 18:32:17 +0600

    first commit

---
+++ no-1.dart
@@ -0,0 +1,20 @@
+// void main() {
+//   var firstname = "Shreya";
+//   var lastname = "Das";
+//   print("full name: $firstname $lastname");
+// }
+
+// void main() {
+//   int num1 = 10;
+//   int num2 = 3;
+
+//   int sum = num1 + num2;
+//   int sub = num1 - num2;
+//   double div = num1 / num2;
+//   print("$sum $sub $div");
+// }
+
+void main() {
+  double price = 10.656576;
+  print(price.toStringAsFixed(2));
+}
